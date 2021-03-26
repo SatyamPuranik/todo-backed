c3613cae7b02dd49cf8f96a52510b483dfb79416
SatyamPuranik <clashersatyam.7@gmail.com>
2021-03-26 22:31:56 +0530

    Login
    Installed cookie-parser

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,16 +22,26 @@ exports.register = async (req, res, next) => {
 };
 
 //@desc      Login user
-//@route     POST /api/v1/auth/register
+//@route     POST /api/v1/auth/login
 //@access    Public
 exports.login = async (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
-  const user = await User.create({
-    name,
-    email,
-    password
-  });
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid Credentials', 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid Credentials', 401));
+  }
 
   const token = user.getSignedJwtToken();
 
